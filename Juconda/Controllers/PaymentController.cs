84b227388babce5338d694e2a53e54c1a6f90c27
Artur Petrosyan <arturpetrosyan152001@gmail.com>
2023-04-23 04:28:22 +0300

    Поменял иконку. Создал пару вьюшек.

---
+++ Juconda/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Juconda.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Juconda/Controllers/ReturnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Juconda.Controllers
+{
+    public class ReturnController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
